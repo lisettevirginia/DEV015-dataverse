0e834fdb3d06950f958d8ac9f52ad942eaf2c64a
Maye85 <mayegabrielaboffil@gmail.com>
2024-06-25 14:14:28 -0500

    metodo reactor hito 2

--- src/view.js
+++ src/view.js
@@ -1,4 +1,13 @@
 export const renderItems = (data) => {
+
+  //1.recibir parametros- 2.crear la lista - 3 recorrer la data -4  crear un li dentro del HTML
+  //1.llamar a dataset.js a travez de un getElementById
+  //2.Con el forEach creamos la lista (innterHTML)
+  //3.crear el li dentro del HTML 
+  //4.celebramos con café
+
+  
+
   console.log(data)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
   return 'example';
